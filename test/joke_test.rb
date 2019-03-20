fc2e225c4e82071666ca8de0ce38c1982dd38e0c
Mills Provosty <millsprovosty@gmail.com>
2019-03-20 09:11:49 -0600

    Iteration 1 complete

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -4,7 +4,17 @@ require './lib/joke'
 
 class JokeTest < Minitest::Test
 
+  def setup
+    @joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+  end
+
   def test_joke_exists
-    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    assert_instance_of Joke, @joke
+  end
+
+  def test_joke_has_attributes
+    assert_equal 1, @joke.id
+    assert_equal "Why did the strawberry cross the road?", @joke.setup
+    assert_equal "Because his mother was in a jam.", @joke.punchline
   end
 end
---
+++ test/user_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+require './lib/user'
+
+class UserTest < Minitest::Test
+  def test_user_exists
+    sal = User.new("Sal")
+  end
+end
