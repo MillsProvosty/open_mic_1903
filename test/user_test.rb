5df62757ec23db2a0cd14d6420fc82e238abd5ff
Mills Provosty <millsprovosty@gmail.com>
2019-03-20 09:15:08 -0600

    initial commit for iteration 2, user class and tests

--- test/user_test.rb
+++ test/user_test.rb
@@ -4,7 +4,22 @@ require './lib/joke'
 require './lib/user'
 
 class UserTest < Minitest::Test
+
+  def setup
+    @sal = User.new("Sal")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+  end
+
   def test_user_exists
-    sal = User.new("Sal")
+    assert_instance_of User, @sal
+  end
+
+  def test_user_has_name
+    assert_equal "Sal", @sal.name
+  end
+
+  def test_jokes_returns_empty_array
+    assert_equal [], @sal.jokes
   end
 end
