04734314d76c9f7cd64197f3140d40f86fe4d6a6
Mills Provosty <millsprovosty@gmail.com>
2019-03-20 09:29:27 -0600

    Iteration 3 complete

--- test/user_test.rb
+++ test/user_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
 require './lib/user'
+require 'pry'
 
 class UserTest < Minitest::Test
 
@@ -9,6 +10,7 @@ class UserTest < Minitest::Test
     @sal = User.new("Sal")
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    @ali = User.new("Ali")
   end
 
   def test_user_exists
@@ -28,4 +30,19 @@ class UserTest < Minitest::Test
     @sal.learn(@joke_2)
     assert_equal [@joke_1, @joke_2], @sal.jokes
   end
+
+  def test_ali_can_learn_jokes_from_sal
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+    assert_equal [@joke_1, @joke_2], @ali.jokes
+  end
+
+  def test_find_joke_by_id
+    @ali.learn(@joke_1)
+    @ali.learn(@joke_2)
+    assert_equal @joke_1, @ali.joke_by_id(1)
+    assert_equal @joke_2, @ali.joke_by_id(2)
+  end
+
+
 end
